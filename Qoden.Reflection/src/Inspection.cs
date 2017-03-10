85d3a7a108b61c4dac38d1234d0bdfade88c679d
Andrey Verbin <info@studiomobile.ru>
2017-03-10 04:25:42 +0300

    Reorganize project

--- Qoden.Reflection/src/Inspection.cs
+++ Qoden.Reflection/src/Inspection.cs
@@ -10,47 +10,44 @@ namespace Qoden.Reflection
 		public static object GetValue (object container, MemberInfo member)
 		{
 			if (container == null)
-				throw new ArgumentNullException ("container");
+				throw new ArgumentNullException (nameof(container));
 			if (member == null)
-				throw new ArgumentNullException ("member");
-			var memberType = member.MemberType;
-			if ((memberType & MemberTypes.Field) != 0) {
+				throw new ArgumentNullException (nameof(member));
+			if (member is FieldInfo) {
 				return ((FieldInfo)member).GetValue (container);
-			} else if ((memberType & MemberTypes.Property) != 0) {
+			} else if (member is PropertyInfo) {
 				return ((PropertyInfo)member).GetValue (container);
 			}
-			throw new ArgumentException ("member");
+			throw new ArgumentException (nameof(member));
 		}
 
 		public static void SetValue (object container, MemberInfo member, object value)
 		{
 			if (container == null)
-				throw new ArgumentNullException ("container");
+				throw new ArgumentNullException (nameof(container));
 			if (member == null)
-				throw new ArgumentNullException ("member");
-			var memberType = member.MemberType;
-			if ((memberType & MemberTypes.Field) != 0) {
+				throw new ArgumentNullException (nameof(member));
+			if (member is FieldInfo) {
 				((FieldInfo)member).SetValue (container, value);
-			} else if ((memberType & MemberTypes.Property) != 0) {
+			} else if (member is PropertyInfo) {
 				((PropertyInfo)member).SetValue (container, value);
 			} else {
-				throw new ArgumentException ("member");
+				throw new ArgumentException (nameof(member));
 			}
 		}
 
 		public static Type GetMemberType(MemberInfo member)
 		{
 			if (member == null)
-				throw new ArgumentNullException ("member");
-			var memberType = member.MemberType;
-			if ((memberType & MemberTypes.Field) != 0) {
+				throw new ArgumentNullException (nameof(member));			
+			if (member is FieldInfo) {
 				var fieldInfo = member as FieldInfo;
 				return fieldInfo.FieldType;
-			} else if ((memberType & MemberTypes.Property) != 0) {
+			} else if (member is PropertyInfo) {
 				var propertyInfo = member as PropertyInfo;
 				return  propertyInfo.PropertyType;
 			}
-			throw new ArgumentException ("member");
+			throw new ArgumentException (nameof(member));
 		}
 
 		public static bool HasAttribute<T>(this MemberInfo member) where T : Attribute
@@ -71,14 +68,14 @@ namespace Qoden.Reflection
 		public static IEnumerable<Attribute> Attributes(this MemberInfo member, Type attributeType)
 		{
 			if (member == null)
-				throw new ArgumentNullException ("member");
+				throw new ArgumentNullException (nameof(member));
 			return member.GetCustomAttributes().Where (a => a.GetType().GetTypeInfo().IsSubclassOf(attributeType));
 		}
 
 		public static PropertyInfo[] InstanceProperties(Type type)
 		{
 			if (type == null)
-				throw new ArgumentNullException ("type");
+				throw new ArgumentNullException (nameof(type));
 			return type.GetProperties (BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
 		}
 
