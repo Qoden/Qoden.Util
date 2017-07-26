033069275d6e0582f182e0054ac39ce4145c6020
Andrey Verbin <info@studiomobile.ru>
2017-07-27 02:18:47 +0300

    Make plugins more testable

--- Qoden.Util/src/Plugin.cs
+++ Qoden.Util/src/Plugin.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Reflection;
 using Qoden.Validation;
 using DeviceInfo = Plugin.DeviceInfo.CrossDeviceInfo;
@@ -7,15 +9,30 @@ namespace Qoden.Util
 {
     public class Plugin 
     {
+        public static Func<string, string, object> PluginProvider;
+
+        private static ConcurrentDictionary<string, object> _plugins = new ConcurrentDictionary<string, object>();
+
+        public static void AddPlugin(string key, object plugin)
+        {
+            _plugins.AddOrUpdate(key, plugin, (_1, _2) => plugin);
+        }
+
         public static T Load<T>(string prefix, string name)
         {
             Assert.Argument(prefix, nameof(prefix)).NotEmpty();
             Assert.Argument(name, nameof(name)).NotEmpty();
 
+            object plugin;
+            if (_plugins.TryGetValue($"{prefix}.{name}", out plugin))
+            {
+                return (T)plugin;
+            }
+
             var deviceInfo = DeviceInfo.Current;
             var assemblyName = new AssemblyName($"{prefix}.{deviceInfo.Platform}");
             var assembly = Assembly.Load(assemblyName);
-            var t = assembly.GetType($"{prefix}.{deviceInfo.Platform}.{name}", true, true);
+            var t = assembly.GetType($"{prefix}.{name}", true, true);
             return (T)Activator.CreateInstance(t);
         }
     }
