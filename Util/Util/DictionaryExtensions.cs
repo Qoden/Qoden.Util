15a64567011c16186e90756dd2b4f6ef07a805bd
Sergey Martynov <martynovs@gmail.com>
2018-04-24 00:08:42 +0300

    fix dictionary helper

--- Util/Util/DictionaryExtensions.cs
+++ Util/Util/DictionaryExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Qoden.Util
 {
@@ -7,9 +6,7 @@ namespace Qoden.Util
     {
         public static V GetValue<K, V>(this IDictionary<K, V> dict, K key, V defaultValue = default(V))
         {
-            V val = defaultValue;
-            dict.TryGetValue(key, out val);
-            return val;
+            return dict.TryGetValue(key, out var value) ? value : defaultValue;
         }
     }
 }
